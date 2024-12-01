c909233fe49b97b3bd22dd415995ecce0cbc788b
lukasz <lukaszbondar10@gmail.com>
2024-12-01 18:27:09 +0100

    Dodanie profilu

--- sklep-frontend/src/pages/ProfilePage.tsx
+++ sklep-frontend/src/pages/ProfilePage.tsx
@@ -1,9 +1,14 @@
 import OrderHistory from "../components/OrderHistory";
+import Profile from "../components/ProfileForm";
+import "../styles/ProfilePage.css"; // Import stylów
 
-
-export default function ProfilePage(){
+export default function ProfilePage() {
     return (
-        <OrderHistory/>
+        <div className="profile-page">
+            <div className="profile-page-container">
+                <Profile />
+                <OrderHistory />
+            </div>
+        </div>
     );
-
 }
